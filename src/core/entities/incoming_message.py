94d35ab168b2da9b8fb2b427ecc600cf5d3f88ad
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-05-14 23:22:58 -0300

    fix case where some users do not have first or last name

--- src/core/entities/incoming_message.py
+++ src/core/entities/incoming_message.py
@@ -1,8 +1,8 @@
+from typing import Optional
 from pydantic import BaseModel
-
 
 class IncomingMessage(BaseModel):
     user_id: int
-    first_name: str
-    last_name: str
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
     text: str
--- src/core/exceptions/exceptions.py
+++ src/core/exceptions/exceptions.py
@@ -3,7 +3,7 @@ from fastapi import status
 
 class UserNotFoundException(Exception):
     def __init__(self, user_id: int, first_name: str, last_name: str):
-        self.detail = f"User {first_name} {last_name} with telegram ID {user_id} not whitelisted. Usage is forbidden"
+        self.detail = f"User {first_name or ''} {last_name or ''} with telegram ID {user_id} not whitelisted. Usage is forbidden"
         self.status_code = status.HTTP_403_FORBIDDEN
 
 
